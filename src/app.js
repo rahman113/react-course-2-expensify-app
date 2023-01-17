be7836ef74f18a8056511559f8f99fdbebc7070b
Ataurrahman <ataurrahman9731@gmail.com>
2023-01-17 10:59:20 +0530

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore()
-
+console.log("testing store");
 // store.dispatch(addExpense({ description: 'Water bill', amount:4500}));
 // store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000}));
 // store.dispatch(addExpense({ description: 'Rent', amount:109500}));
